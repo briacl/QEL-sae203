3aa1ee17a9d78fd49bc23fcb139445f7cab5235b
briacl <briac.le_meillat@kerbra.com>
2023-03-29 08:10:48 +0200

    wip webhook

--- application/pages/github_webhook.php
+++ application/pages/github_webhook.php
@@ -2,19 +2,21 @@
 $secret = "";
 
 $path = "/srv/www/sae203";
-$gitreturn = shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
 
-// Headers deliveried from GitHub
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+// Headers qui est envoyé par GitHub
+$signature = isset($_SERVER['HTTP_X_HUB_SIGNATURE']) ? $_SERVER['HTTP_X_HUB_SIGNATURE'] : false;
 
+
+//Work in progress !!! pour le momnent si la signature n'est pas présente on fait le else
 if ($signature) {
-  $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
-  if (strcmp($signature, $hash) == 0) {
-    echo shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
-    exit();
-  }
+    $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+    if (strcmp($signature, $hash) == 0) {
+        $gitreturn = shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
+    }
+}else { //TODO: à supprimer quand le webhook sera opérationnel
+    $gitreturn = shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
 }
 
-echo $blade->run('github_webhook',['gitreturn'=>$gitreturn]);
+echo $blade->run('github_webhook', ['gitreturn' => $gitreturn]);    //On appelle le template blade (github_webhoo.blade.php) en lui passant la variable $gitreturn en tant que 'gitreturn'
 
 ?>
