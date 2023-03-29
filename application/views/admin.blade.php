0ad2930f5d9f48360834a800817e2246834123e1
briacl <briac.le_meillat@kerbra.com>
2023-03-29 18:25:51 +0200

    select avec autoselect

--- application/views/admin.blade.php
+++ application/views/admin.blade.php
@@ -4,7 +4,13 @@
 <h2>Administration du questionnaire</h2>
 <h3>{!!make_icon('pencil')!!}{{$questionnaire['titre']}}</h3>
 <h3>{!!make_icon('calendar')!!} Clôture à {{format_date($questionnaire['fin'])}}</h3>
-
+<h3>{!!make_icon('task')!!}<label for="statut">Statut</label>
+        <select name="statut" id="statut">
+            <option value="creation" {{ $questionnaire['statut'] == 'creation' ? "selected" : "" }}>Création</option>
+            <option value="publie" {{ $questionnaire['statut'] == 'publie' ? "selected" : "" }}>Publié</option>
+            <option value="termine" {{ $questionnaire['statut'] == 'termine' ? "selected" : "" }}>Terminé</option>
+        </select></h3>
+      <h3>{{$questionnaire['statut']}}
 <h2>Questions : </h2>
 <ul>
 @foreach($questions as $question)
