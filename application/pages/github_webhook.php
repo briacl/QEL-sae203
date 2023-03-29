74705afdc2aa4f0f4ab299eef0a46afc188c0447
briacl <briac.le_meillat@kerbra.com>
2023-03-29 08:30:15 +0200

    secret

--- application/pages/github_webhook.php
+++ application/pages/github_webhook.php
@@ -1,14 +1,21 @@
 <?php
-$secret = "";
+/*
+* Petite méthode d'intégration continue du repos github.com/briacl/sae203
+*   remarque, le repos étant privé la clef ssh locale est autorisée chez github
+*/
+$secret = "ElleEstFunCetteSAE!";
 
 $path = "/srv/www/sae203";
 
 // Headers qui est envoyé par GitHub
 $signature = isset($_SERVER['HTTP_X_HUB_SIGNATURE']) ? $_SERVER['HTTP_X_HUB_SIGNATURE'] : false;
 
-
 //Work in progress !!! pour le momnent si la signature n'est pas présente on fait le else
 if ($signature) {
+    // info: file_get_contents("php://input") est un raccourci pour récupérer tout le formulaire post envoyé par github
+    // hash_hmac('sha1', file_get_contents("php://input"), $secret) calcule donc une signature sur l'ensemble des éléments
+    // du formulaire en provenance de github
+    // cette signature est comparée à celle envoyée en entête dans HTTP_X_HUB_SIGNATURE, si OK on fait un git pull
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
     if (strcmp($signature, $hash) == 0) {
         $gitreturn = shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
