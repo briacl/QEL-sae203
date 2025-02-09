30b790a3cf847793701b447968f7aeea4caccc28
briacl <briac.le_meillat@kerbra.com>
2025-02-09 17:33:45 +0100

    correction102 de ServeurWebController

--- application/pages/ServeurWebController.php
+++ application/pages/ServeurWebController.php
@@ -1,13 +1,13 @@
 <?php
 
-use application/core/Controller.php';
-use Illuminate\Http\Request;
+require_once 'application/core/Controller.php';
 use Illuminate\Support\Facades\DB;  // Pour utiliser DB et effectuer des requêtes SQL brutes
 
 class ServeurWebController extends Controller
 {
     public function index()
     {
+        error_log('ServeurWebController index method called');
         // Exécuter une requête SQL pour récupérer les données de la table 'Tracks' dans la base secondaire
         $data = DB::connection('second_db')->select('SELECT * FROM Tracks');  // 'second_db' est la connexion à la base secondaire
 
@@ -17,6 +17,7 @@ class ServeurWebController extends Controller
 
     public function apiServeurweb()
     {
+        error_log('ServeurWebController apiServeurweb method called');
         // Exécuter la même requête pour récupérer les données en format JSON
         $data = DB::connection('second_db')->select('SELECT * FROM Tracks');
 
