38c9fb2106cc070660f9f59b0ef775ea45bebae8
briacl <briac.le_meillat@kerbra.com>
2025-02-09 17:12:24 +0100

    correction 2 de routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'application/core/routing.php';
-use ServeurWebController;
+require_once 'application/pages/ServeurWebController.php';
 
 
 // Configuration des routes, c'est à dire l'association entre les pages demandées
